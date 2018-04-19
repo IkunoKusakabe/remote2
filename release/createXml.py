54c3f9cca47c9e3c9820551f6b29500badf60d55
Kusakabe <ts-ikuno.kusakabe@rakuten.com>
2018-04-19 18:08:58 +0900

    path

--- release/createXml.py
+++ release/createXml.py
@@ -11,7 +11,7 @@ import ConfigParser
 class XmlCreator:
 	# iniファイル読み込み(py2ver)
 	conf = ConfigParser.SafeConfigParser()
-	conf.read('${JENKINS_HOME}/workspace/salesforce_deploy/pyconf.ini')
+	conf.read('../salesforce_deploy/pyconf.ini')
 	# 出力先
 	BUILD_XML = 'buildToTest.xml'
 	PACKAGE_XML = 'package.xml'
